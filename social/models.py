4e10b4b5267b94828d5e57509713c5a114d06720
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-03-02 23:22:57 +0600

    updated

--- social/models.py
+++ social/models.py
@@ -48,7 +48,7 @@ class UserProfile(models.Model):
     picture = models.ImageField(upload_to='uploads/profile_pictures', default='uploads/profile_pictures/default.png', blank=True)
     followers = models.ManyToManyField(User, blank=True, related_name='followers')
 
-
+#review
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
